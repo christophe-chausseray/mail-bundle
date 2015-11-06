a0d3136827c6af22c0ea48ce8d1e95ec3eb83d19
Christophe Chausseray <christophec@iron-mail.net>
2015-11-06 09:27:58 +0000

    Create send grid mailer service

--- Mailer/SendGridMailer.php
+++ Mailer/SendGridMailer.php
@@ -10,76 +10,24 @@ class SendGridMailer
     protected $sendGrid;
 
     /**
-     * @var Email $mail
-     */
-    protected $mail;
-
-    /**
-     * @var string $subject
-     */
-    protected $subject;
-
-    /**
-     * @var string $body
-     */
-    protected $body;
-
-    /**
-     * @var array $attachments
-     */
-    protected $attachments;
-
-    /**
      * @param SendGrid $sendGrid
-     * @param Email    $mail
      */
-    public function __construct(SendGrid $sendGrid, Email $mail)
+    public function __construct(SendGrid $sendGrid)
     {
         $this->sendGrid = $sendGrid;
-        $this->mail     = $mail;
     }
 
     /**
-     * Set Subject
-     *
-     * @param string $subject
-     *
-     * @return $this
-     */
-    public function setSubject($subject)
-    {
-        $this->subject = $subject;
-
-        return $this;
-    }
-
-    /**
-     * @param string $attachment
-     */
-    public function addAttachment($attachment)
-    {
-        $this->attachments[] = $attachment;
-    }
-
-    /**
-     * @param string $to
      * @param string $from
-     *
-     * @throws \SendGrid\Exception
+     * @param string $to
+     * @param string $subject
+     * @param string $body
+     * @param array  $options
      */
-    public function send($to, $from)
+    public function send($from, $to, $subject, $body, array $options)
     {
-        $this->mail->addTo($to)
-            ->setFrom($from)
-            ->setFromName($from)
-            ->setSubject($this->subject)
-            ->setHtml($this->body)
-            ->setText(htmlspecialchars($this->body));
+        $mail = new Email($from, $to, $subject, $body);
 
-        if (!empty($this->attachments)) {
-            $this->mail->setAttachments($this->attachments);
-        }
-
-        $this->sendGrid->send($this->mail);
+        $this->sendGrid->send($mail, $options);
     }
 }
