2e43d00c56248efb0b54deb98daa16b153358a28
Christophe Chausseray <christophec@iron-mail.net>
2015-11-06 12:12:55 +0000

    Fix name configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('common_mail');
+        $rootNode = $treeBuilder->root('mail_layer');
 
         $rootNode
             ->children()
